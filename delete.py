618bad901a554d355ba672a63190e59c3bf21acf
Steve Halladay <steve.halladay@datastax.com>
2019-10-21 20:54:43 +0000

    made output consistent with other crud ops

--- delete.py
+++ delete.py
@@ -2,10 +2,15 @@
 from db_connection import Connection
 
 # this is a delete statement in python
-connection = Connection()
-output = connection.session.execute(
-    "DELETE FROM killrvideo.user_credentials WHERE email = %s",
-    ['cv@datastax.com']
-)
-print(output)
-connection.close()
+try:
+    connection = Connection()
+    output = connection.session.execute(
+        "DELETE FROM killrvideo.user_credentials WHERE email = %s",
+        ['cv@datastax.com']
+    )
+except:
+    print('Failure')
+else:
+    print('Success')
+finally:
+    connection.close()
