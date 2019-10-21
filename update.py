6ea775af613ea90f888aa079634c06263a303459
Steve Halladay <steve.halladay@datastax.com>
2019-10-21 20:50:10 +0000

    made output consistent with other crud ops

--- update.py
+++ update.py
@@ -2,10 +2,15 @@
 from db_connection import Connection
 
 # this is a update statement in python
-connection = Connection()
-output = connection.session.execute(
-    "UPDATE killrvideo.user_credentials SET password = %s WHERE email = %s",
-    ['Cr1st1n@sN3wP@ssW0rd', 'cv@datastax.com']
-)
-print(output)
-connection.close()
+try:
+    connection = Connection()
+    connection.session.execute(
+        "UPDATE killrvideo.user_credentials SET password = %s WHERE email = %s",
+        ['Cr1st1n@sN3wP@ssW0rd', 'cv@datastax.com']
+    )
+except:
+    print('Failure')
+else:
+    print('Success')
+finally:
+    connection.close()
